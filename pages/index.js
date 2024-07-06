5004784da097449b6d27fec8e3a91d9ccc97775b
Ismael Monteiro <87835141+Ismaelthe10@users.noreply.github.com>
2024-07-06 21:08:25 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-function Home(){
-    return <h1>O Senhor é o meu pastor, nada me faltará. -Salmos 23:1 🙏</h1>
+function Home() {
+  return <h1>O Senhor é o meu pastor, nada me faltará. -Salmos 23:1 🙏</h1>;
 }
-
 export default Home;
