d47a436dded1d996b9f45368515632328a412d18
Ismael Monteiro <87835141+Ismaelthe10@users.noreply.github.com>
2025-04-15 17:04:14 +0000

    fix: add `'` to utc in create-users.js

--- infra/migrations/1744565288384_create-users.js
+++ infra/migrations/1744565288384_create-users.js
@@ -26,12 +26,12 @@ exports.up = (pgm) => {
     created_at: {
       type: "timestamptz",
       notNull: true,
-      default: pgm.func("timezone('utc, now())"),
+      default: pgm.func("timezone('utc', now())"),
     },
     updated_at: {
       type: "timestamptz",
       notNull: true,
-      default: pgm.func("timezone('utc, now())"),
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
