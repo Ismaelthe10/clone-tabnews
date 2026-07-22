e4fa8b7bb21ec7bf12ea40453d5bc6708cebbc70
Ismael Monteiro <87835141+Ismaelthe10@users.noreply.github.com>
2026-07-22 02:51:38 +0000

    chore: update email in `registration-flow.test.js`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -42,7 +42,7 @@ describe("Use case: Registration Flow (all sucessfull)", () => {
   test("Receive activation email", async () => {
     const lastEmail = await orchestrator.getLastEmail();
 
-    expect(lastEmail.sender).toBe("<ismael-dev@outlook.com>");
+    expect(lastEmail.sender).toBe("<ismael-dev@ismael-dev.com.br>");
     expect(lastEmail.recipients[0]).toBe("<registration.flow@curso.dev>");
     expect(lastEmail.subject).toBe("Ative seu cadastro!");
     expect(lastEmail.text).toContain("RegistrationFlow");
